653eba7c1d573256eb14cfde446a94615f0196bf
An Tran <an.tran2131@gmail.com>
2019-08-30 08:29:26 -0400

    implement cmf analysis methods.

--- studies.py
+++ studies.py
@@ -1,5 +1,6 @@
 # This module defines the properties of each study and their associated calculations.
 import pandas as pd
+import functools
 
 
 class Study_Area:
@@ -25,27 +26,65 @@ class Study_Area:
         self.end_year = end_year
         self.input_cmfs = pd.read_excel(input_cmfs)
 
-    def count_segments(self):
+    def count_segments(self) -> int:
         """
         Returns the number of segments in this study area.
         """
-        pass
+        return len(self.input_cmfs.Segment.unique())
 
-    def get_mp_range(self):
+    def get_segment_names(self) -> tuple:
+        """
+        Returns tuple of distinct segment names.
+        """
+        return self.input_cmfs.Segment.unique()
+
+    def get_mp_range(self) -> tuple:
         """
         Returns the milepost range of this study area.
         """
-        pass
+        return (self.input_cmfs.Start_MP.min(), self.input_cmfs.End_MP.max())
 
-    def get_cmfs(
+    def calculate_crash_cmfs(
         self,
         crash_milepost: float,
-        severity: str,
-        crash_type: str,
-        crash_dir: str,
-        crash_time: str,
+        severity: str = "All",
+        crash_type: str = "All",
+        crash_dir: str = "All",
+        crash_time: str = "All",
     ) -> list:
         """
-        Returns the CMF value(s) that match the given input parameters at the crash milepost.
+        Returns the product of all input CMF value(s) that match the given input parameters at the crash milepost.
         """
-        pass
+        coefficients = []
+        results = self.input_cmfs.query(
+            f"Start_MP <= {crash_milepost} and {crash_milepost} < End_MP", inplace=False
+        )
+        cmf_stack = results.iloc[0 : len(results), 6]
+        for row in range(len(results)):
+            if (
+                (
+                    results[row]["Severity"].lower() == "all"
+                    or results[row]["Severity"].lower() == severity.lower()
+                )
+                and (
+                    results[row]["Crash_Type"].lower() == "all"
+                    or results[row]["Crash_Type"] == crash_type.lower()
+                )
+                and (
+                    results[row]["Direction"].lower() == "all"
+                    or results[row]["Direction"] == crash_dir.lower()
+                )
+                and (
+                    results[row]["Time"].lower() == "all"
+                    or results[row]["Time"] == crash_time.lower()
+                )
+            ):
+                coefficients.append(cmf_stack.pop(row))
+        return coefficients
+
+    @staticmethod
+    def reduce_cmfs(cmfs: list) -> float:
+        """
+        Return product of all CMF values in cmfs list.
+        """
+        return functools.reduce(lambda a, b: a * b, cmfs)
